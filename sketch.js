3b51e63201e28837bc32904ba67fb09dee4b71da
ParshwaGundecha <88487547+ParshwaGundecha@users.noreply.github.com>
2021-09-22 14:12:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,43 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var ball;
+var wall;
+var right_wall;
+var left_wall;
+
 function preload()
 {
-	
 }
 
 function setup() {
-	createCanvas(800, 700);
-
+	createCanvas(windowWidth,windowHeight);
 
 	engine = Engine.create();
 	world = engine.world;
+  
+  wall=new Ground(width/2,670,width,10);
+  World.add(world,wall);
+  
+  right_wall=new Ground(900,600,10,100);
+  World.add(world,right_wall);
 
-	//Create the Bodies Here.
+  left_wall=new Ground(1100,600,10,100);
+  World.add(world,left_wall);
 
+	var ball_options={
+   isStatic:false,
+	 restitution:0.3,
+	 friction:0,
+	 density:1.2,	
+	};
 
-	Engine.run(engine);
+  //Create the Bodies Here.
+  ball=Bodies.circle(200,-50,20,ball_options);
+  World.add(world,ball); 
+  Engine.run(engine);
   
 }
 
@@ -28,9 +46,21 @@ function draw() {
   rectMode(CENTER);
   background(0);
   
+  push ();
+  ellipse(ball.position.x,ball.position.y,20);
+  pop ();
+  wall.show();
+  right_wall.show();
+  left_wall.show();
   drawSprites();
- 
+  
+  keyPressed();
 }
 
+function keyPressed(){
+  if(keyCode===UP_ARROW){
+   Matter.Body.applyForce(ball,{x:0,y:0},{x:2,y:2})
+  }
 
+}
 
